75b13345b75eddaae3939ba0d5844f087815376d
Mikael Fidjeland <mikael@sirktek.no>
2025-11-28 12:43:42 +0100

    fix: PMD unneccessary import [skip-ci]

--- src/main/java/no/sirktek/taxonomy/loader/FurnitureRdfsTaxonomyLoader.java
+++ src/main/java/no/sirktek/taxonomy/loader/FurnitureRdfsTaxonomyLoader.java
@@ -3,7 +3,7 @@ package no.sirktek.taxonomy.loader;
 /**
  * Loads the furniture taxonomy from RDF-S Turtle files using Apache Jena
  */
-public class FurnitureRdfsTaxonomyLoader extends no.sirktek.taxonomy.loader.RdfsTaxonomyLoader {
+public class FurnitureRdfsTaxonomyLoader extends RdfsTaxonomyLoader {
 
     private static final String FURNITURE_NAMESPACE = "http://taxonomy.sirktek.no/furniture#";
     private static final String RESOURCE_PATH = "/taxonomy/furniture-base.ttl";
